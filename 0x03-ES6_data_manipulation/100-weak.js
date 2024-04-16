1141665b658187877c8cce554722186f2a1855d4
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-04-16 22:00:30 +0200

    task advanced

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,12 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
-    const endpointData = weakMap.get(endpoint);
+    let endpointData = weakMap.get(endpoint);
     if (endpointData >= 5) {
       throw new Error('Endpoint load is high');
     }
-    weakMap.set(endpoint, (endpointData + 1));
+    endpointData = endpointData ? endpointData + 1 : 1;
+    weakMap.set(endpoint, (endpointData));
   } else {
     weakMap.set(endpoint, 1);
   }
