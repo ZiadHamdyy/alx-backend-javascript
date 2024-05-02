6806fc7f947d8b010aa90b313e69929a268cc542
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-02 16:05:03 +0300

    task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const Array = [];
+  for (const idx of array) {
+    Array.push(`${appendString}${idx}`);
+  }
+  return Array;
+}
